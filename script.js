841eb7f0a1cdf778390dee8118cbd4eab6083a40
akyeshi <akyeshi@gmail.com>
2021-09-06 23:09:37 -0400

    assignment 5 done

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 // Write your JavaScript code here!
 
+// firing load event for window object
 window.addEventListener("load", function() {
-    let form = document.querySelector("form").reset(); 
+    let form = document.querySelector("form")
+    form.reset(); 
+    // hide the content of missionTarget div id with 'faultyItems' list of launch status messages 
+    document.getElementById("faultyItems").style.visibility = "hidden"; 
+    // create data object to store form data input
     form.addEventListener("submit", function(event){
         let pilotData = document.querySelector("input[name=pilotName]").value;
         let copilotData = document.querySelector("input[name=copilotName]").value;
         let fuelLevelData = document.querySelector("input[name=fuelLevel]").value; 
         let cargoLevelData = document.querySelector("input[name=cargoMass]").value;
         let list = document.getElementById("faultyItems"); 
-
+        // update display messages in launchStatuscheck div 
         formSubmission(document, list, pilotData, copilotData, fuelLevelData, cargoLevelData); 
+        // cancel what form submit button does
         event.preventDefault(); 
     }); 
 
-    let listedPlanets;      // container
+    // json object returned from API call 
+    let listedPlanets;    
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
     let listedPlanetsResponse = myFetch(); 
     listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
         console.log(listedPlanets);
 
+        // storing specific data of the randomly chosen planet 
         let planet = pickPlanet(listedPlanets); 
         let planetName = planet["name"]; 
         let planetDiameter = planet["diameter"]; 
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,6 +1,7 @@
 // Write your helper functions here!
 require('isomorphic-fetch');
 
+// display API data for missionTarget div
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
     let missionTarget = document.getElementById("missionTarget"); 
     missionTarget.innerHTML = 
@@ -15,9 +16,9 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
             <li>Number of Moons: ${moons}</li>
         </ol>
         <img src="${imageUrl}">`
-
 }
 
+// method for launchForm div to return for user edge case input
 function validateInput(testInput) {
     if (testInput === ""){
         return "Empty"
@@ -28,44 +29,65 @@ function validateInput(testInput) {
     } 
 }
 
+// method for LaunchStatusCheck div to validate form data inputs and display launch status message
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    document.getElementById("pilotStatus").innerHTML = `Pilot is ready for launch`; 
-    document.getElementById("copilotStatus").innerHTML = `Co-pilot is ready for launch`; 
+    // reset launchStatusCheck div innerHTML for each form submission
+    list.style.visibility = "hidden"; 
+    document.getElementById("pilotStatus").innerHTML = `Pilot Ready`; 
+    document.getElementById("copilotStatus").innerHTML = `Co-pilot Ready`; 
     document.getElementById("fuelStatus").innerHTML = `Fuel level high enough for launch`; 
     document.getElementById("cargoStatus").innerHTML = `Cargo mass low enough for launch`; 
     document.getElementById("launchStatus").innerHTML = `Awaiting Information Before Launch`; 
-    document.getElementById("launchStatus").style.color = "black"; 
+    document.getElementById("launchStatus").style.color = "#000000"; 
 
-
+    // data validation
     let pilotInputType = validateInput(pilot); 
     let copilotInputType = validateInput(copilot); 
     let fuelLevelInputType = validateInput(fuelLevel); 
     let cargoLevelInputType = validateInput(cargoLevel); 
 
+    // default boolean set for valid form field data
     let isValidInputType = true; 
-    if (pilotInputType === "Empty" && pilotInputType === 'Is a Number'|| 
-        copilotInputType === "Empty" && copilotInputType === 'Is a Number' || 
-        fuelLevelInputType === "Empty" && fuelLevelInputType === 'Not a Number' || 
-        cargoLevelInputType === "Empty" && cargoLevelInputType === 'Not a Number'){
-        alert ('All fields are required!'); 
-        isValidInputType = false; 
-    } else if (isValidInputType) {
-        document.getElementById("pilotStatus").innerHTML = `Pilot ${pilot} is ready for launch`; 
-        document.getElementById("copilotStatus").innerHTML = `Co-pilot ${copilot} is ready for launch`; 
-    } else if (fuelLevel < 10000) {
-        document.getElementById("fuelStatus").innerHTML = `Fuel level too low for launch`; 
-        document.getElementById("launchStatus").innerHTML = `Shuttle Not Ready for Launch`; 
-        document.getElementById("launchStatus").style.color = "#ff0000"; 
-        isValidInputType = false; 
-    } else if (cargoLevel > 10000) {
-        document.getElementById("cargoStatus").innerHTML = `Cargo mass too heavy for launch`; 
-        document.getElementById("launchStatus").innerHTML = `Shuttle Not Ready for Launch`; 
-        document.getElementById("shuttleStatus").style.color = "#ff0000"; 
-        isValidInputType = false; 
-    } else if (isValidInputType) {
-        document.getElementById("launchStatus").style.color = "#008000"; 
-        document.getElementById("launchStatus").innerHTML = `Shuttle is Ready for Launch`; 
+
+    // verify all form field data has proper data type entered
+    if ((pilotInputType === "Empty") || (copilotInputType === "Empty") || (fuelLevelInputType === "Empty") || (cargoLevelInputType === "Empty")) {
+        alert("All fields are required!");
+        isValidInputType = false;
     } 
+
+    else if ((pilotInputType !== "Not a Number") || (copilotInputType !== "Not a Number") || 
+    (fuelLevelInputType !== "Is a Number") || (cargoLevelInputType !== "Is a Number")) {
+        alert("Make sure to enter valid information for each field!");
+        isValidInputType = false;
+    }
+
+    // if form field data input type is valid, then check if fuelLevel & cargoLevel passes test
+    if (isValidInputType) {
+        document.getElementById("pilotStatus").innerHTML = `Pilot ${pilot} is ready for launch`; 
+        document.getElementById("copilotStatus").innerHTML = `Co-pilot ${copilot} is ready for launch`;
+
+        if (fuelLevel < 10000) {
+            document.getElementById("fuelStatus").innerHTML = `Fuel level too low for launch`; 
+            document.getElementById("launchStatus").innerHTML = `Shuttle Not Ready for Launch`; 
+            document.getElementById("launchStatus").style.color = "#ff0000"; 
+            isValidInputType = false;
+        }
+        
+        if (cargoLevel > 10000) {
+            document.getElementById("cargoStatus").innerHTML = `Cargo mass too heavy for launch`; 
+            document.getElementById("launchStatus").innerHTML = `Shuttle Not Ready for Launch`; 
+            document.getElementById("shuttleStatus").style.color = "#ff0000"; 
+            isValidInputType = false;
+        }
+
+        // form data type passed, cargoLevel & fuelLevel passed test, display proper message
+        if (isValidInputType) {
+            document.getElementById("launchStatus").style.color = "#008000"; 
+            document.getElementById("launchStatus").innerHTML = `Shuttle is Ready for Launch`; 
+        } 
+        // launchStatusCheck div make visible all launch status messages
+        list.style.visibility = "visible"; 
+    }
 }
 
 async function myFetch() {
@@ -76,6 +98,7 @@ async function myFetch() {
     return planetsReturned;
 }
 
+// missionTarget div display randomly fetched API data of planets 
 function pickPlanet(planets) {
     return planets[Math.floor(Math.random()*planets.length)]; 
 }
